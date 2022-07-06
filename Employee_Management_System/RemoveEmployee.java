9931fb75925b3873fe28e59793c3af1dda792306
Mayukh <107843066+MayukhNag@users.noreply.github.com>
2022-07-06 16:36:24 +0530

    Update RemoveEmployee.java

--- Employee_Management_System/RemoveEmployee.java
+++ Employee_Management_System/RemoveEmployee.java
@@ -101,7 +101,7 @@ public class RemoveEmployee extends JFrame implements ActionListener {
         back.addActionListener(this);
         add(back);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/delete.png"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("com/Employee_Management_System/icons/delete.png"));
         Image i2 = i1.getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
@@ -135,3 +135,4 @@ public class RemoveEmployee extends JFrame implements ActionListener {
         new RemoveEmployee();
     }
 }
+
